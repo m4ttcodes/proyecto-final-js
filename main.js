5b989a173fbf645a209d46302a7b36300a301779
Vicente Aburto <vicenteaburtog@gmail.com>
2022-08-31 21:42:10 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+document.querySelector('h3')
+console.log(document.querySelector('h3'))
+console.log(document.querySelector('.h3-omega'))
+// console.log(document.querySelectorAll('.h3-omega'))
+// console.log(document.querySelector('#parrafo'))
+// console.log(document.getElementById("parrafo"))
+
+// const parrafo = document.querySelector('#parrafo')
+// parrafo.innerHTML = 'Texto desde JS'
+// parrafo.classList.add('h3-omega')
