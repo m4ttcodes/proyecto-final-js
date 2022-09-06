797ebf0bab5e07d724ce46118faf01809d1d0e8a
Vicente Aburto <vicenteaburtog@gmail.com>
2022-09-06 05:12:16 -0400

    Añadiendo json, bootstrap y funciones

--- main.js
+++ main.js
@@ -1,10 +1,37 @@
-document.querySelector('h3')
-console.log(document.querySelector('h3'))
-console.log(document.querySelector('.h3-omega'))
-// console.log(document.querySelectorAll('.h3-omega'))
-// console.log(document.querySelector('#parrafo'))
-// console.log(document.getElementById("parrafo"))
+const items = document.getElementById('items')
+const templateCard = document.getElementById('template-card').content
+const fragment = new DocumentFragment
 
-// const parrafo = document.querySelector('#parrafo')
-// parrafo.innerHTML = 'Texto desde JS'
-// parrafo.classList.add('h3-omega')
+document.addEventListener('DOMContentLoaded', () => {
+  fetchData()
+})
+items.addEventListener('click', e => {
+  addCarrito(e)
+})
+
+const fetchData = async () => {
+  try {
+    const res = await fetch('./json/api.json')
+    const data = await res.json()
+    console.log(data)
+    mostrarCards(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const mostrarCards = data => {
+   data.forEach(producto => {
+      templateCard.querySelector('h5').textContent = producto.title
+      templateCard.querySelector('p').textContent = producto.precio
+      templateCard.querySelector('img').setAttribute('src', producto.thumbnailUrl)
+      templateCard.querySelector('.btn-dark').dataset.id = producto.id
+      const clone = templateCard.cloneNode(true)
+      fragment.appendChild(clone)
+   });
+   items.appendChild(fragment)
+}
+
+const addCarrito = e => {
+  console.log(e.target)
+}
